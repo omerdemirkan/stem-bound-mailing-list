ac60ffd4c52ee54bba35819b5d411d5a5ad5e4af
omerdemirkan <omerfarukpiano@gmail.com>
2020-07-04 11:22:20 -0700

    add post to mailing list functionality

--- main.js
+++ main.js
@@ -10,16 +10,23 @@ if (storedMailingListEmail) handleAlreadySubmitted();
 
 function submitForm() {
     const emailInput = form.elements["email-input"].value;
-    console.log(emailInput);
+    console.log("yoo");
     if (!isValidEmail(emailInput)) {
         errorMessage.innerText = "Invalid email";
     } else {
         errorMessage.innerText = "";
         // To simulate an api call.
-        setTimeout(function () {
-            localStorage.setItem("mailingListEmail", emailInput);
-            handleAlreadySubmitted();
-        }, 1000);
+
+        signUp(emailInput)
+            .then(function (result) {
+                localStorage.setItem("mailingListEmail", emailInput);
+                handleAlreadySubmitted();
+                console.log(result);
+            })
+            .catch(function (error) {
+                errorMessage.innerText = "An error occured";
+                console.log(error);
+            });
     }
 }
 
@@ -30,4 +37,25 @@ function isValidEmail(s) {
 function handleAlreadySubmitted() {
     form.style = "display: none;";
     signUpConfirmationMessageBox.style = "display: block;";
+}
+
+function signUp(email) {
+    return fetch(
+        "https://stem-bound-api-4ea6ol7fuq-uc.a.run.app/api/mailing-list",
+        {
+            method: "POST",
+            headers: {
+                Accept: "application/json",
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ email: email }),
+        }
+    ).then(async function (response) {
+        const data = await response.json();
+        if (response.ok) {
+            return data;
+        } else {
+            return Promise.reject(data);
+        }
+    });
 }
