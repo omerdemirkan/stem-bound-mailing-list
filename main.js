1233dddaa481d3fba51e095a51ccbee2c15b169f
omerdemirkan <omerfarukpiano@gmail.com>
2020-09-07 13:14:08 -0700

    add loading functionality to user sign up

--- main.js
+++ main.js
@@ -6,9 +6,12 @@ const errorMessage = document.getElementById("error-message");
 
 const storedMailingListEmail = localStorage.getItem("mailingListEmail");
 
+let fetchLoading = false;
+
 if (storedMailingListEmail) handleAlreadySubmitted();
 
 function submitForm() {
+    if (fetchLoading) return;
     const email = form.elements["email-input"].value;
     const role = form.elements["user-role"].value;
     if (!isValidEmail(email)) {
@@ -21,6 +24,7 @@ function submitForm() {
 
         signUp({ email, role })
             .then(function (result) {
+                fetchLoading = false;
                 localStorage.setItem("mailingListEmail", email);
                 handleAlreadySubmitted();
                 console.log(result);
@@ -42,6 +46,8 @@ function handleAlreadySubmitted() {
 }
 
 function signUp(userData) {
+    fetchLoading = true;
+    errorMessage.innerText = "Signing up...";
     return fetch(
         "https://stem-bound-api-4ea6ol7fuq-uc.a.run.app/api/v1/mailing-list",
         // "http://localhost:8080/api/v1/mailing-list",
